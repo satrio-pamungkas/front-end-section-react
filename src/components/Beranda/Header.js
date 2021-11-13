bb70f8134843a22bb19e59f2146e36fa75f31a25
AkbarMahmudin <wastanami17@gmail.com>
2021-11-13 23:03:28 +0700

    create register and speakers components

--- src/components/Beranda/Header.js
+++ src/components/Beranda/Header.js
@@ -1,19 +1,23 @@
 import Logo from '../../assets/img/LogoSection.svg';
 
 export const Header = () => {
-    return (
-        <div className="row">
-            <div className="col-3"></div>
-            <div className="col-6">
-                <img className="logo-section" src={Logo} alt="LogoSection"></img>
-                <h1 className="title">The Future of Finance is <strong>Fintech</strong></h1>
-                <h1 className="subtitle-form">4 - 12 DESEMBER 2021</h1>
-                <span>
-                    <button className="btn btn-warning daftar">DAFTAR PESERTA</button>
-                    <button className="btn btn-warning agenda">LIHAT AGENDA</button>
-                </span>
-            </div>
-            <div className="col-3"></div>
+  return (
+    <div id="header">
+      <div className="row">
+        <div className="col-3"></div>
+        <div className="col-6">
+          <img className="logo-section" src={Logo} alt="LogoSection"></img>
+          <h1 className="title">
+            The Future of Finance is <strong>Fintech</strong>
+          </h1>
+          <h1 className="subtitle-form">4 - 12 DESEMBER 2021</h1>
+          <span>
+            <button className="btn btn-warning daftar">DAFTAR PESERTA</button>
+            <button className="btn btn-warning agenda">LIHAT AGENDA</button>
+          </span>
         </div>
-    );
-}
+        <div className="col-3"></div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Beranda/Register.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../../assets/img/logo-section-vol2.png';
+
+export const Register = () => {
+  return (
+    <div id="register">
+      <div className="container text-center">
+        <img src={Logo} className="img-fluid my-4" alt="Logo Section Vol 2" />
+
+        <div>
+          <a href="#register" className="btn btn-primary my-4">
+            Daftar Sekarang
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
