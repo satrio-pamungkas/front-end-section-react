17957a23e70e3995c808dddec123dfda4240a573
Muhammad Raihan Satrio <adewipo@gmail.com>
2021-11-16 18:55:17 +0700

    chore: restructure file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Navbar } from './components/Beranda/Navbar';
+import { Navbar } from './components/common/Navbar';
+import { Footer } from './components/common/Footer';
 import { Beranda } from './pages/Beranda';
 import { FAQ } from './pages/FAQ';
+import { Register } from './pages/Register';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
@@ -12,11 +14,13 @@ library.add(fab, faEnvelope, faYoutube, faInstagram);
 export const App = () => {
     return (
         <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Beranda/>}/>
-                <Route path="/faq" element={<FAQ/>}/>
-            </Routes>
+            <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Beranda/>}/>
+                    <Route path="/faq" element={<FAQ/>}/>
+                    <Route path="/daftar" element={<Register/>}/>
+                </Routes>
+            <Footer/>
         </BrowserRouter>
     );
 }
