8e69cb41b97111ba8d1f716cb7d58a30129cdb9d
Muhammad Raihan Satrio <adewipo@gmail.com>
2021-11-19 10:47:51 +0700

    feat: added Register component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,30 +4,22 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 
 export const Register = () => {
-    // const validationSchema = Yup.object().shape({
-    //     name: Yup.string()
-    //         .required('Nama wajib diisi'),
-    //     jenisKelamin: Yup.string()
-    //         .required('Jenis kelamin wajib diisi'),
-    //     email: Yup.string()
-    //         .required('Alamat surel wajib diisi')
-    //         .email('Alamat surel tidak valid'),
-    //     noWhatsapp: Yup.string()
-    //         .required('Nomor WhatsApp wajib diisi'),
-    //     institusi: Yup.string()
-    //         .required('Institusi wajib diisi'),
-    //     profesi: Yup.string()
-    //         .required('Profesi wajib diisi'),
-    //     domisili: Yup.string()
-    //         .required('Domisili wajib diisi')
-    // });
-
     const validationSchema = Yup.object().shape({
         name: Yup.string()
             .required('Nama wajib diisi'),
+        jenisKelamin: Yup.string()
+            .required('Jenis kelamin wajib diisi'),
         email: Yup.string()
             .required('Alamat surel wajib diisi')
-            .email('Alamat surel tidak valid')
+            .email('Alamat surel tidak valid'),
+        noWhatsapp: Yup.string()
+            .required('Nomor WhatsApp wajib diisi'),
+        institusi: Yup.string()
+            .required('Institusi wajib diisi'),
+        profesi: Yup.string()
+            .required('Profesi wajib diisi'),
+        domisili: Yup.string()
+            .required('Domisili wajib diisi')
     });
 
     const {
@@ -47,32 +39,90 @@ export const Register = () => {
         <div className="register">
             <div className="container register">
                 <Alert/>
-                <form className="form" onSubmit={handleSubmit(onSubmit)}>
-                    <div className="form-group name">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
+                <form className="form" onSubmit={handleSubmit(onSubmit)} noValidate="novalidate">
+                    <div className="form-floating">
                         <input 
                             name="name"
-                            type="text" 
-                            className="form-control" 
-                            id="validationServer03" 
-                            placeholder="Alamat Surel" 
-                            aria-describedby="validationServer03Feedback" 
-                            {...register('name')} 
+                            type="text"
+                            className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+                            id="floatingInputInvalid" 
+                            placeholder="Nama Lengkap" 
+                            {...register('name')}
                         />
-                        <div id="validationServerUsernameFeedback" className="invalid-feedback">
-                            Please choose a username.
+                        <label htmlFor="floatingInputInvalid">Nama Lengkap</label>
+                        <div className="invalid-feedback">
+                            {errors.name?.message}
                         </div>
                     </div>
-                    <div className="form-floating email">
+                    <div className="form-floating">
                         <input 
                             name="email"
-                            type="email" 
-                            className="form-control"
+                            type="email"
+                            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
                             id="floatingInputInvalid" 
-                            placeholder="name@example.com" 
+                            placeholder="Alamat Surel" 
                             {...register('email')}
                         />
-                        <label htmlFor="floatingInputInvalid">Invalid input</label>
+                        <label htmlFor="floatingInputInvalid">Alamat Surel</label>
+                        <div className="invalid-feedback">
+                            {errors.email?.message}
+                        </div>
+                    </div>
+                    <div className="form-floating">
+                        <input 
+                            name="noWhatsapp"
+                            type="number"
+                            className={`form-control ${errors.noWhatsapp ? 'is-invalid' : ''}`}
+                            id="floatingInputInvalid" 
+                            placeholder="Nomor WhatsApp" 
+                            {...register('noWhatsapp')}
+                        />
+                        <label htmlFor="floatingInputInvalid">Nomor WhatsApp</label>
+                        <div className="invalid-feedback">
+                            {errors.noWhatsapp?.message}
+                        </div>
+                    </div>
+                    <div className="form-floating">
+                        <input 
+                            name="institusi"
+                            type="text"
+                            className={`form-control ${errors.institusi ? 'is-invalid' : ''}`}
+                            id="floatingInputInvalid" 
+                            placeholder="Institusi" 
+                            {...register('institusi')}
+                        />
+                        <label htmlFor="floatingInputInvalid">Institusi</label>
+                        <div className="invalid-feedback">
+                            {errors.institusi?.message}
+                        </div>
+                    </div>
+                    <div className="form-floating">
+                        <input 
+                            name="profesi"
+                            type="text"
+                            className={`form-control ${errors.profesi ? 'is-invalid' : ''}`}
+                            id="floatingInputInvalid" 
+                            placeholder="Profesi atau Jabatan" 
+                            {...register('profesi')}
+                        />
+                        <label htmlFor="floatingInputInvalid">Profesi atau Jabatan</label>
+                        <div className="invalid-feedback">
+                            {errors.profesi?.message}
+                        </div>
+                    </div>
+                    <div className="form-floating">
+                        <input 
+                            name="domisili"
+                            type="text"
+                            className={`form-control ${errors.domisili ? 'is-invalid' : ''}`}
+                            id="floatingInputInvalid" 
+                            placeholder="Domisili" 
+                            {...register('domisili')}
+                        />
+                        <label htmlFor="floatingInputInvalid">Domisili</label>
+                        <div className="invalid-feedback">
+                            {errors.domisili?.message}
+                        </div>
                     </div>
                     <button type="submit" className="btn btn-warning">Daftar</button>
                 </form>
