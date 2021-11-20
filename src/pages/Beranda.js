e60f06efc985a70107c1d8d4e4cd06affaad7ee5
huda16 <mochamadnurulhuda16@gmail.com>
2021-11-20 07:34:04 +0700

    menambah komponen timeline dan memperbaiki responsive header

--- src/pages/Beranda.js
+++ src/pages/Beranda.js
@@ -4,6 +4,7 @@ import { Register } from '../components/Beranda/Register';
 import { Speakers } from '../components/Beranda/Speakers';
 import { Acara } from '../components/Beranda/Acara';
 import { Kontak } from '../components/Beranda/Kontak';
+import { Timeline } from '../components/Beranda/Timeline';
 
 export const Beranda = () => {
   return (
@@ -11,6 +12,7 @@ export const Beranda = () => {
       <Header />
       <CountdownSection />
       <Acara />
+      <Timeline />
       <Speakers />
       <Register />
       <Kontak />
